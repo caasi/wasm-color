339a200bbe6379607234dc11b63acb223404378b
caasi Huang <caasi.igd@gmail.com>
2017-10-08 23:30:55 +0800

    Use `spectest-interp` instead of `wasm-interp` to run spec tests
    see: https://github.com/WebAssembly/wabt/pull/643

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # build and test math functions
 wast2wasm --spec ./math.wast -o ./math.json
-wasm-interp --spec ./math.json
+spectest-interp ./math.json
 
 # rgb2hsv
 wast2wasm --spec ./rgb2hsv.wast -o ./rgb2hsv.json
 # link and overwrite the output wasm
 wasm-link ./math.0.wasm ./rgb2hsv.0.wasm -o ./rgb2hsv.0.wasm
-wasm-interp --spec ./rgb2hsv.json
+spectest-interp ./rgb2hsv.json
 
 # hsv2rgb
 wast2wasm --spec ./hsv2rgb.wast -o ./hsv2rgb.json
 wasm-link ./math.0.wasm ./hsv2rgb.0.wasm -o ./hsv2rgb.0.wasm
-wasm-interp --spec ./hsv2rgb.json
+spectest-interp ./hsv2rgb.json
