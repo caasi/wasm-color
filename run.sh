9d00deb76716e694b744a09b85379c21c102d6da
caasi Huang <caasi.igd@gmail.com>
2020-07-05 00:37:34 +0800

    Migrate to the latest toolkit

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # build and test math functions
-wast2wasm --spec ./math.wast -o ./math.json
+wast2json ./math.wast -o ./math.json
 spectest-interp ./math.json
 
 # rgb2hsv
-wast2wasm --spec ./rgb2hsv.wast -o ./rgb2hsv.json
+wast2json ./rgb2hsv.wast -o ./rgb2hsv.json
 # link and overwrite the output wasm
 wasm-link ./math.0.wasm ./rgb2hsv.0.wasm -o ./rgb2hsv.0.wasm
 spectest-interp ./rgb2hsv.json
 
 # hsv2rgb
-wast2wasm --spec ./hsv2rgb.wast -o ./hsv2rgb.json
+wast2json ./hsv2rgb.wast -o ./hsv2rgb.json
 wasm-link ./math.0.wasm ./hsv2rgb.0.wasm -o ./hsv2rgb.0.wasm
 spectest-interp ./hsv2rgb.json
